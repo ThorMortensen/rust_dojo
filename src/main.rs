a58b7218dd1e38d7118183d29d0661611d4711ba
thm <thm@rovsing.dk>
2022-05-17 15:45:36 +0200

    Added pretty print

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use std::collections::HashMap;
-
-use std::convert;
-use std::ops::{Index, IndexMut};
-
+extern crate hex;
+// use std::rc;
+// use std::convert;
+// use std::ops::{Index, IndexMut};
+use pad::{Alignment, PadStr};
 
 // macro_rules! dbg {
 //     ($x:expr) => {
@@ -10,19 +11,21 @@ use std::ops::{Index, IndexMut};
 //     }
 // }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone)]
 pub struct Field {
     bit_width: u32,
     bit_mask: u32,
     data: u32,
+    name: String,
 }
 
 impl Field {
-    fn new(bit_width: u32, data: u32) -> Self {
+    fn new(bit_width: u32, data: u32, name: String) -> Self {
         Self {
             bit_width: bit_width,
             bit_mask: 0xffffffff >> (32 - bit_width),
             data: data,
+            name: name,
         }
     }
 
@@ -56,22 +59,21 @@ pub struct Pkg {
     header_bits: usize,
     pkg_compiled: Vec<u8>,
     is_compiled: bool,
-
 }
 
-impl Index<usize> for Pkg {
-    type Output = Field;
+// impl Index<usize> for Pkg {
+//     type Output = Field;
 
-    fn index<'a>(&'a self, i: usize) -> &'a Field {
-        &self.field_vec[i]
-    }
-}
+//     fn index(&self, i: usize) -> Field {
+//         &self.field_vec[i]
+//     }
+// }
 
-impl IndexMut<usize> for Pkg {
-    fn index_mut<'a>(&'a mut self, i: usize) -> &'a mut Field {
-        &mut self.field_vec[i]
-    }
-}
+// impl IndexMut<usize> for Pkg {
+//     fn index_mut(&'a mut self, i: usize) -> &'a mut Field {
+//         &mut self.field_vec[i]
+//     }
+// }
 
 impl Pkg {
     pub fn new() -> Self {
@@ -88,18 +90,17 @@ impl Pkg {
     pub fn add_field(&mut self, name: &str, bit_width: u32, data: u32) {
         self.header_bits += bit_width as usize;
         self.is_compiled = false;
-        self.field_vec.push(Field::new(bit_width, data));
-        self.field_map
-            .insert(name.to_string(), self.field_vec.len());
+        self.field_vec
+            .push(Field::new(bit_width, data, name.to_owned()));
+        self.field_map.insert(name.to_owned(), self.field_vec.len());
     }
 
     pub fn get_field_mut(&mut self, name: &str) -> Option<&mut Field> {
-        match self.field_map.get(name) {
-            Some(v) => {
-                self.is_compiled = false;
-                return self.field_vec.get_mut(*v);
-            }
-            None => return None,
+        if let Some(v) = self.field_map.get(name) {
+            self.is_compiled = false;
+            return self.field_vec.get_mut(*v);
+        } else {
+            return None;
         }
     }
 
@@ -136,41 +137,46 @@ impl Pkg {
     }
 
     fn compile(&mut self) {
-
-
-        let mut pkg = Vec::with_capacity(self.header_size() + self.payload.len() + WORK_SPACE);
-        pkg.extend(iter)
-                let mut bits_used: u32 = 0;
+        let mut pkg = vec![0; self.header_size() + self.payload.len() + WORK_SPACE];
+        let mut bits_used: u32 = 0;
 
         for field in &self.field_vec {
-            
             let reminder: u32 = bits_used % 8;
-            let shifted = (field.get_data().clone() as u64) << (64 - (reminder + field.get_bit_width())); 
+            let shifted =
+                (field.get_data().clone() as u64) << (64 - (reminder + field.get_bit_width()));
 
-            let tail = bits_used as usize / 8;  
-            // Merge into header  
-            pkg[tail + 0] |= ((shifted >> 56) & 0xff) as u8; // Merge 
-            pkg[tail + 1] |= ((shifted >> 48) & 0xff) as u8; // Inset
-            pkg[tail + 2] |= ((shifted >> 40) & 0xff) as u8; // Inset
-            pkg[tail + 3] |= ((shifted >> 32) & 0xff) as u8; // Inset
-            pkg[tail + 4] |= ((shifted >> 24) & 0xff) as u8; // Reminder (if any)
+            let tail = bits_used as usize / 8;
+            // Merge into header
+            pkg[tail + 0] |= ((shifted >> 56) & 0xff) as u8; // Merge
+            pkg[tail + 1] = ((shifted >> 48) & 0xff) as u8; // Inset
+            pkg[tail + 2] = ((shifted >> 40) & 0xff) as u8; // Inset
+            pkg[tail + 3] = ((shifted >> 32) & 0xff) as u8; // Inset
+            pkg[tail + 4] = ((shifted >> 24) & 0xff) as u8; // Reminder (if any)
             bits_used += field.get_bit_width();
-
-
-
-            // let field_byte_width = (field.get_bit_width() / 8) + 1; 
-            
-
-            // for bytes in 0..field_byte_width as usize{
-            //     pkg[tail + bytes] = ((shifted >> (64 - (8 * bytes))) & 0xff) as u8; // Inset
-
-            // }
         }
-        pkg[self.header_size()..].copy_from_slice(&mut self.payload[..]);
+        pkg[self.header_size()..self.header_size() + self.payload.len()]
+            .copy_from_slice(&self.payload[..]);
         self.is_compiled = true;
         self.pkg_compiled = pkg;
     }
 
+    fn print(&mut self) {
+        println!("=========================");
+        println!("Pkg size    : {} bytes", self.size());
+        println!("Heder size  : {} bytes", self.header_size());
+        println!("Payload size: {} bytes", self.payload.len());
+        println!("-------- Fields ----------");
+        println!("Name      : bits, value ");
+        for field in &self.field_vec {
+            println!(
+                "{:10}: {:2},   0x{:0X} ",
+                &field.name, &field.bit_width, &field.data
+            )
+        }
+        println!("------------------------");
+        println!("Bytes: {}", hex::encode(self.to_bytes()));
+        println!("{}", "header | payload".pad(15 + (self.header_size() * 2),' ', Alignment::Right, false));
+    }
     // pub fn from_bytes(&mut self, bytes: &Vec<u8>) {
     //     let mut asInt;
 
@@ -195,14 +201,16 @@ fn main() {
     pkg.add_field("fieldpre", 4, 4);
     pkg.add_field("field1", 1, 1);
     pkg.add_field("field2", 1, 1);
-    pkg.add_field("field3", 1,1);
+    pkg.add_field("field3", 1, 1);
     pkg.add_field("AAfield4", 1, 1);
+    pkg.add_payload(vec![1, 2, 3, 4, 5]);
 
-    let mut t: u64 = 0x123456789ABCDEF; 
+    pkg.print();
 
-    // From<u64> 
+    let mut t: u64 = 0x123456789ABCDEF;
 
-    let data_out = pkg.to_bytes();
-    dbg!(data_out);
+    // From<u64>
 
+    // let data_out = pkg.to_bytes();
+    // // dbg!(data_out);
 }
